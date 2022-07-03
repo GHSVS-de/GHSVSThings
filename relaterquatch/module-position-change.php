50b1256a9a858c17a6e219dbf6fd690e0e3f7924
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-07-03 16:19:23 +0200

    Update module-position-change.php

--- relaterquatch/module-position-change.php
+++ relaterquatch/module-position-change.php
@@ -7,7 +7,7 @@ https://forum.joomla.de/index.php/Thread/4004-JModuleHelper-getModule-findet-Mod
 
 public function onAfterModuleList($modules)
 {
- 
+
  if (JFactory::getApplication()->isClient('administrator'))
  {
   return;
@@ -16,25 +16,39 @@ public function onAfterModuleList($modules)
  // Auf welcher Seite bin ich? Plump. Sonst muss man halt Menüs abfragen o.ä.
  $currentPageId = JFactory::getApplication()->input->get('Itemid');
  // echo 'DEBUG $currentPageId: '.print_r($currentPageId,true);exit;
- 
+
  // Plump. Hartkodiert. In welchen Menü-Ids Position austauschen.
  $exchangeIn = array(435, 272, 254, 862);
 
  if (in_array($currentPageId, $exchangeIn))
  {
   //echo 'DEBUG To do: '.print_r('Change position!', true);exit;
-  
+
   foreach ($modules as $module)
   {
-   
+
    // Weitere Abfrage-Möglichkeit $module->title.
    if ($module->module == 'mod_custom' && $module->position == 'position-8')
    {
     // Modulpos. neu setzen.
     $module->position = 'banner';
     //echo 'DEBUG DONE: '.print_r('Position exchanged!', true);exit;
-    
+
    }
   }
  }
 }
+
+/*
+Und so könnten die Felder für Konfiguration aussehen, die man den Modulen
+zusätzlich unterjubeln muss, z.B. via onContentPrepareForm im selben System-Plugin wie oben.
+*/
+<field name="modulePosition" type="ModulesPositionedit"
+	client="site"
+	label="PLG_SYSTEM_BS3GHSVS_MODULEPOSITION"/>
+
+<field name="modulePositionMenuItems" type="Menuitem"
+	multiple="true" layout="joomla.form.field.groupedlist-fancy-select"
+	label="PLG_SYSTEM_BS3GHSVS_MODULEPOSITION_MENUITEMS">
+	<option value="0">JNONE</option>
+</field>
